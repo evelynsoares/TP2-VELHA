79d7aa73dd4438bd4b9aa977de7d9647137f2dbe
evelynsoares <soares.evelynp@gmail.com>
2025-06-11 21:10:41 -0300

    teste jogo invalido3

--- teste_velha.cpp
+++ teste_velha.cpp
@@ -215,6 +215,14 @@ TEST(VerificarJogoTest, Invalido2) {
     };
     ASSERT_EQ(verificar_jogo(jogo), -2);
 }
+TEST(VerificarJogoTest, Invalido3) {
+    jogoVelha jogo = {
+        {2, 2, 2},
+        {1, 1, 1},
+        {0, 0, 0}
+    };
+    ASSERT_EQ(verificar_jogo(jogo), -2);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv); // Inicializa o GTest
